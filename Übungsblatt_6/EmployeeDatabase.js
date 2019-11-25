779f5aa79b4653c4252ef5d390f50d3b7ec38116
Alex-Posa <50545422+Alex-Posa@users.noreply.github.com>
2019-11-25 12:56:45 +0100

    Update EmployeeDatabase.js

--- Übungsblatt_6/EmployeeDatabase.js
+++ Übungsblatt_6/EmployeeDatabase.js
@@ -3,12 +3,45 @@ var employeeDatabase = (function() {
 	var employees = [];
 	
     function createEmployee(name,email,room) {
-		var newEmployee = new Employee(employees.length, name, email, room);
-		employees.push(newEmployee);
-        return newEmployee();
+		var id = createId();
+		var nextEmployee = new Employee(id, name, email, room);
+		employees.push(nextEmployee);
+        return nextEmployee;
     }
     
-    function updateEmployee() {
+	function createId(){
+		var id = 0; 
+		
+		for(var i=0; i < employees.length; i++){
+			if(employees[i].id === id){
+				id++;
+			}
+		}
+		
+		return id;
+	}
+	
+	function idExists(id,name){
+		
+		for(var i = 0; i < employees.length; i++){
+			if(employees[i].id === id && employees[i].name === name){
+				return i;
+			} else {
+				console.log("Not a legitimate ID");
+				return 1;
+			}
+		}
+	}
+	
+	
+    function updateEmployee(id, name, email, room) {
+		
+		var id = idExists(id,name);
+		if(id != 1){
+			var newEmployee = new Employee(id, name, email, room);
+			employees[id] = newEmployee;
+			return newEmployee;
+		} 
         return 0;
     }
     
